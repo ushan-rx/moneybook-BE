23fd5a361ae482281174690908eed4dc0d9b4967
ushan senarathna <ushansenarathna000@gmail.com>
2025-01-03 08:36:40 +0530

    init

---
+++ src/test/java/com/moneybook/MoneybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneybookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
