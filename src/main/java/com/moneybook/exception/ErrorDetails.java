6189a6235fff54b2eb36e8ada9f14292ccaa6b09
ushan senarathna <ushansenarathna000@gmail.com>
2025-01-04 22:38:02 +0530

    feat: add exception handling

---
+++ src/main/java/com/moneybook/exception/ErrorDetails.java
@@ -0,0 +1,18 @@
+package com.moneybook.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+}
---
+++ src/main/java/com/moneybook/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.moneybook.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
