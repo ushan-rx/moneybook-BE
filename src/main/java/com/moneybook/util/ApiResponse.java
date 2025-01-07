6dc34d55ef2700526de99099002e06ea628abd38
ushan senarathna <ushansenarathna000@gmail.com>
2025-01-07 17:22:57 +0530

    feat: add standard API response

---
+++ src/main/java/com/moneybook/util/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.moneybook.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse <T>{
+    private LocalDateTime timestamp;
+    private int status;
+    private T data; // Generic type for the response data
+    private String message;
+    private String error; // Optional, used only when there's an error
+    private String path;
+}
