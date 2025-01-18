8a51181e468ce4d49f6ce93911d5a58eac725fd2
ushan senarathna <ushansenarathna000@gmail.com>
2025-01-18 14:04:26 +0530

    chore : config spring security with no auth

---
+++ src/main/java/com/moneybook/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.moneybook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/**").permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
