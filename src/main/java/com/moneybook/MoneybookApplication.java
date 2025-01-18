c07b7b385eff5c8ef3bfac51a3805671c17a0eb1
ushan senarathna <ushansenarathna000@gmail.com>
2025-01-18 14:07:50 +0530

    feat : add scheduling

--- src/main/java/com/moneybook/MoneybookApplication.java
+++ src/main/java/com/moneybook/MoneybookApplication.java
@@ -2,8 +2,10 @@ package com.moneybook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MoneybookApplication {
 
     public static void main(String[] args) {
